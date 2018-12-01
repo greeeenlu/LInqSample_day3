60ceff1b7a7488b690022a5736d091ddc749bb07
green.lu <green.lu@titansoft.com.sg>
2018-12-01 14:49:42 +0800

    union test

--- ZipSample/test/UnionTests.cs
+++ ZipSample/test/UnionTests.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ZipSample.test
 {
@@ -11,18 +12,34 @@ namespace ZipSample.test
         [TestMethod]
         public void Union_integers()
         {
-            var first = new List<int> { 1, 3, 5 };
+            var first = new List<int> { 1, 3, 3, 5 };
             var second = new List<int> { 5, 3, 7, 9 };
 
             var expected = new List<int> { 1, 3, 5, 7, 9 };
 
-            var actual = MyUnion(first, second);
+            var actual = MyUnion(first, second).ToList();
             expected.ToExpectedObject().ShouldEqual(actual);
         }
 
         private IEnumerable<int> MyUnion(IEnumerable<int> first, IEnumerable<int> second)
         {
-            throw new NotImplementedException();
+            var firstEnumerator = first.GetEnumerator();
+            var secondEnumerator = second.GetEnumerator();
+            var result = new HashSet<int>();
+            while (firstEnumerator.MoveNext())
+            {
+                result.Add(firstEnumerator.Current);
+            }
+            while (secondEnumerator.MoveNext())
+            {
+                result.Add(secondEnumerator.Current);
+            }
+
+            var resultEnumerator = result.GetEnumerator();
+            while (resultEnumerator.MoveNext())
+            {
+                yield return resultEnumerator.Current;
+            }
         }
     }
 }
