ddb2e7950981c4488945a12f68598c5d65c315ff
green.lu <green.lu@titansoft.com.sg>
2018-12-01 15:20:16 +0800

    union girls test

--- ZipSample/test/UnionTests.cs
+++ ZipSample/test/UnionTests.cs
@@ -17,15 +17,40 @@ namespace ZipSample.test
 
             var expected = new List<int> { 1, 3, 5, 7, 9 };
 
-            var actual = MyUnion(first, second).ToList();
+            var actual = MyUnion<int>(first, second).ToList();
+            expected.ToExpectedObject().ShouldEqual(actual);
+        }
+        [TestMethod]
+        public void Union_Girls()
+        {
+            var first = new List<Girl>
+            {
+                new Girl(){Name="lulu",Age = 18},
+                new Girl(){Name="lily",Age = 12},
+            };
+            var second = new List<Girl>
+            {
+                new Girl(){Name="leo",Age = 17},
+                new Girl(){Name="lily",Age = 12},
+
+            };
+
+            var expected = new List<Girl>
+            {
+                new Girl(){Name="lulu",Age = 18},
+                new Girl(){Name="lily",Age = 12},
+                new Girl(){Name="leo",Age = 17},
+            };
+
+            var actual = MyUnion<Girl>(first, second,new MyComparer()).ToList();
             expected.ToExpectedObject().ShouldEqual(actual);
         }
 
-        private IEnumerable<int> MyUnion(IEnumerable<int> first, IEnumerable<int> second)
+        private IEnumerable<TSource> MyUnion<TSource>(IEnumerable<TSource> first, IEnumerable<TSource> second)
         {
             var firstEnumerator = first.GetEnumerator();
             var secondEnumerator = second.GetEnumerator();
-            var result = new HashSet<int>();
+            var result = new HashSet<TSource>();
             while (firstEnumerator.MoveNext())
             {
                 if (result.Add(firstEnumerator.Current))
@@ -40,6 +65,41 @@ namespace ZipSample.test
                 {
                     yield return secondEnumerator.Current;
                 }
+            }
+        }
+        private IEnumerable<TSource> MyUnion<TSource>(IEnumerable<TSource> first, IEnumerable<TSource> second, IEqualityComparer<TSource> myComparer)
+        {
+            var firstEnumerator = first.GetEnumerator();
+            var secondEnumerator = second.GetEnumerator();
+            
+            var result = new HashSet<TSource>(myComparer);
+            while (firstEnumerator.MoveNext())
+            {
+                if (result.Add(firstEnumerator.Current))
+                {
+                    yield return firstEnumerator.Current;
+                }
+                
+            }
+            while (secondEnumerator.MoveNext())
+            {
+                if (result.Add(secondEnumerator.Current))
+                {
+                    yield return secondEnumerator.Current;
+                }
+            }
+        }
+
+        class MyComparer : IEqualityComparer<Girl>
+        {
+            public bool Equals(Girl x, Girl y)
+            {
+                return x.Name == y.Name && x.Age == y.Age;
+            }
+
+            public int GetHashCode(Girl obj)
+            {
+                return obj.Name.GetHashCode() + obj.Age.GetHashCode();
             }
         }
     }
