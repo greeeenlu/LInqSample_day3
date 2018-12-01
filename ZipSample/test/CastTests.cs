01f247d8c86edfff9f9d5d400d043e33b7c95ef0
green.lu <green.lu@titansoft.com.sg>
2018-12-01 14:22:40 +0800

    pass cast string

--- ZipSample/test/CastTests.cs
+++ ZipSample/test/CastTests.cs
@@ -15,27 +15,38 @@ namespace ZipSample.test
         public void cast_integers()
         {
             var arrayList = new ArrayList { 2, 6 };
-            var actual = MyCast(arrayList).ToList();
+            var actual = MyCast<int>(arrayList).ToList();
 
             var expected = new List<int> { 2, 6 };
             expected.ToExpectedObject().ShouldEqual(actual);
         }
 
         [TestMethod]
+        public void cast_string()
+        {
+            var arrayList = new ArrayList { "2", "6" };
+            var actual = MyCast<string>(arrayList).ToList();
+
+            var expected = new List<string> { "2", "6" };
+            expected.ToExpectedObject().ShouldEqual(actual);
+        }
+
+
+        [TestMethod]
         //[ExpectedException(typeof(InvalidCastException))]
         public void cast_integers_with_cast_failed()
         {
             var arrayList = new ArrayList { 2, "4", 6 };
-            Action action = () => MyCast(arrayList).ToList();
+            Action action = () => MyCast<int>(arrayList).ToList();
             action.Should().Throw<InvalidCastException>();
         }
 
-        private IEnumerable<int> MyCast(IEnumerable arrayList)
+        private IEnumerable<TSource> MyCast<TSource>(IEnumerable arrayList)
         {
             var enumerator = arrayList.GetEnumerator();
             while (enumerator.MoveNext())
             {
-                yield return (int)enumerator.Current;
+                yield return (TSource)enumerator.Current;
             }
         }
     }
