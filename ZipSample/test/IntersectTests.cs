d00e7cec25c866889bfef05db50b682a3f77f4c4
green.lu <green.lu@titansoft.com.sg>
2018-12-01 15:59:33 +0800

    intersect test

--- ZipSample/test/IntersectTests.cs
+++ ZipSample/test/IntersectTests.cs
@@ -11,8 +11,8 @@ namespace ZipSample.test
         [TestMethod]
         public void intersect_integers()
         {
-            var first = new List<int> { 1, 3, 5 };
-            var second = new List<int> { 5, 3, 7, 9 };
+            var first = new List<int> { 1, 3, 3, 5 };
+            var second = new List<int> { 5, 5, 3, 7, 9 };
 
             var expected = new List<int> { 3, 5 };
 
@@ -22,7 +22,27 @@ namespace ZipSample.test
 
         private IEnumerable<int> MyIntersect(IEnumerable<int> first, IEnumerable<int> second)
         {
-            throw new System.NotImplementedException();
+            var firstEnumerator = first.GetEnumerator();
+            var secondEnumerator = second.GetEnumerator();
+            var hashSet = new HashSet<int>();
+            var alreadyReturn = new HashSet<int>();
+            while (firstEnumerator.MoveNext())
+            {
+                if (hashSet.Add(firstEnumerator.Current) == false 
+                    && alreadyReturn.Add(firstEnumerator.Current))
+                {
+                    yield return firstEnumerator.Current;
+                }
+            }
+
+            while (secondEnumerator.MoveNext())
+            {
+                if (hashSet.Add(secondEnumerator.Current) == false 
+                    && alreadyReturn.Add(secondEnumerator.Current))
+                {
+                    yield return secondEnumerator.Current;
+                }
+            }
         }
     }
 }
